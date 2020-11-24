70ea183f3dc0a57768f23052e313f184f04db780
Albert Chen <adc054@eng.ucsd.edu>
2020-11-23 16:35:31 -0800

    prep c3

--- random_tests/movezero.js
+++ random_tests/movezero.js
@@ -1,16 +1,27 @@
-var moveZeroes = function(nums) {
-        let count = 0;
-        for (let i = 0; i < nums.length; i++){
-            if(nums[i] !== 0){
-                nums[count++] = nums[i];
-            }
-            console.log(nums)
+function moveZeroes(nums) {
+    let counter = 0;
+    for (let i = 0; i < nums.length; i++){
+        if(nums[i] !== 0){
+            nums[counter] = nums[i];
+            counter++;
         }
+    }
 
-        // for (let i = count; i < nums.length; i++){
-        //     nums[i] = 0;
-        // }
-        return nums;    
-    };
+    for (let i = counter; i < nums.length; i++){
+        nums[i] = 0;
+    }
+    return nums;    
+};
 
-console.log(moveZeroes([1,1,3,0,12]))
+// var moveZeroes = function(nums) {
+//   nums.sort((a,b) => {return a-b});
+//    for(let index = nums.length - 1; index >= 0 ; index--){
+//     if(nums[index] === 0){
+//       nums.splice(index, 1);
+//       nums.push(0);
+//     }
+//    }
+//    return nums;
+// };
+
+console.log(moveZeroes([1,3,2,0,12 ,13]))
