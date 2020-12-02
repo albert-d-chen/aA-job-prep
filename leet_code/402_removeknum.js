67010c748aa100b84947545d0368bfd242b00b9e
Albert Chen <adc054@eng.ucsd.edu>
2020-12-01 17:21:23 -0800

    do leetcode

---
+++ leet_code/402_removeknum.js
@@ -0,0 +1,34 @@
+var removeKdigits = function(num, k) {
+    
+    if (num.length === k) return '0';
+    
+    let nums = [];
+    let removed = 0;
+    
+    for (let n of num) {
+        while (nums.length && n < nums[nums.length - 1] && removed < k) {
+            nums.pop();
+            removed++;
+        }
+        
+        nums.push(n);
+    }
+    
+    while (removed < k) {
+        nums.pop();
+        removed++;
+    }
+    
+    while (nums.length && nums[0] === '0') {
+        nums.shift();
+    }
+    
+    if (nums.length) {
+        return nums.join('');
+    } else {
+        return '0';
+    }
+    // return nums.join('');
+    
+    
+};
