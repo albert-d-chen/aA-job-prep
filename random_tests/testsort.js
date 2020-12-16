5347881c3702d44c6af56b21ba291e23189018a3
Albert Chen <adc054@eng.ucsd.edu>
2020-12-15 16:37:28 -0800

    test problem

--- random_tests/testsort.js
+++ random_tests/testsort.js
@@ -4,12 +4,31 @@ function test(array) {
 }
 
 let array = ['8:00AM', '8:20PM', '9:00AM', '6:00AM', '6:00PM']
-console.log(test(array));
+// console.log(test(array));
 
 const racer = function () {
     setImmediate(() => console.log('immd'));
     setTimeout(() => console.log('time'), 0);
     process.nextTick(() => console.log('tick'));
+    console.log('current')
 }
 
-console.log(racer());
+const racer1 = function () {
+    setTimeout(() => console.log('time'), 0);
+    setImmediate(() => console.log('immd'));
+    process.nextTick(() => console.log('tick'));
+}
+const racer2 = function () {
+    process.nextTick(() => console.log('tick'));
+    setTimeout(() => console.log('time'), 0);
+    setImmediate(() => console.log('immd'));
+}
+const racer3 = function () {
+    setImmediate(() => console.log('immd'));
+    process.nextTick(() => console.log('tick'));
+    setTimeout(() => console.log('time'), 0);
+}
+
+racer1();
+racer2();
+racer3();
