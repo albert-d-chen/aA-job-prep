97e8282fa98aa22075b474d5c3a7a19330889acf
Albert Chen <adc054@eng.ucsd.edu>
2021-01-14 21:26:45 -0800

    do tictactoe

--- tictactoe/tictactoe.js
+++ tictactoe/tictactoe.js
@@ -7,32 +7,40 @@ function combinations () {
     ]
 
     for (let i = 0; i < board.length; i++) {
-        board[i][0] = 'X';
-        count += fill(board, count, 'O');
-        board = [
-            [], [], [],
-            [], [], [],
-            [], [], [],
-        ]
+        let newBoard = [...board]
+        newBoard[i][0] = 'X';
+        // console.log(newBoard);
+        count += fill(newBoard, count, 'O');
+        // board = [
+        //     [], [], [],
+        //     [], [], [],
+        //     [], [], [],
+        // ]
     }
     return count;
 }
 
 function fill(board, count, move) {
-    if (move === 'X') {
-        move = 'O';
-    } else {
-        move = 'X';
-    }
+    let nextMove;
     console.log(count);
-    for (let i = 0; i < board.length; i++) {
-        if (won(board, move) || full(board)) {
-            count++;
-            return count;
-        }
-        board[i][0] = move;
-        fill(board, count, move);
+
+    if (won(board, move) || full(board)) {
+        count++;
+        return count;
     }
+
+    if (move === 'X') {
+        nextMove = 'O';
+    } else {
+        nextMove = 'X';
+    }
+
+    for (let i = 0; i < board.length; i++) {
+        board[i + 1][0] = nextMove;
+        count += fill(board, count, nextMove);
+    }
+
+    return count;
 }
 
 function full(board) {
@@ -69,10 +77,10 @@ let board = [
             [], [], [],
         ];
 
-        
+
 let count = 0;
 let move = 'X';
-// console.log(combinations());
-console.log(won(board, 'X'));
+console.log(combinations());
+// console.log(won(board, 'X'));
 // console.log(full(board));
-console.log(fill(board, count, move));
+// console.log(fill(board, count, move));
