61a1ae32d2c15741db7356ecde98f28117e4948b
Albert Chen <adc054@eng.ucsd.edu>
2021-01-28 17:33:37 -0800

    do leetcode

---
+++ leet_code/79_word_search.js
@@ -0,0 +1,41 @@
+var exist = function(board, word) {
+    for (let i = 0; i < board.length; i++) {
+        let row = board[i];
+        for (let j = 0; j < row.length; j++) {
+            let letter = board[i][j];
+            let wordCombo = '';
+            wordCombo += letter
+            return helper(i, j, wordCombo, word);
+        }
+    }
+};
+
+function helper(i, j, wordCombo, word) {
+    let directions = [[-1, 0], [0, -1], [0, 1], [1, 0]]
+    console.log(directions[0][0]);
+    for (let k = 0; k < directions.length; k++) {
+        let moveX = directions[k][0] + i;
+        let moveY = directions[k][1] + j;
+        let nextLetter = board[moveX][moveY];
+        wordCombo += nextLetter;
+
+        if (wordCombo === word) {
+            return true;
+        } else {
+            if (i === word.length - 1 && j === word.length - 1) {
+                return false;
+            }
+            return helper(i, j, wordCombo, word);
+        }
+
+    }
+
+}
+
+
+let board = [['A', 'B', 'C', 'E'], 
+             ['S', 'F', 'C', 'S'], 
+             ['A', 'D', 'E', 'F']
+    ]
+let word = 'ABCB';
+console.log(exist(board, word))
