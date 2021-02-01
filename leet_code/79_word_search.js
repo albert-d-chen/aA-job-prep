315d8dc3ec2cb806e74711618711c57fe9ddc05d
Albert Chen <adc054@eng.ucsd.edu>
2021-02-01 14:20:48 -0800

    do leetcode

--- leet_code/79_word_search.js
+++ leet_code/79_word_search.js
@@ -5,15 +5,19 @@ var exist = function(board, word) {
             let letter = board[i][j];
             let wordCombo = '';
             wordCombo += letter
-            return helper(i, j, wordCombo, word);
+            return helper(i, j, wordCombo, word, board);
         }
     }
 };
 
-function helper(i, j, wordCombo, word) {
+function helper(i, j, wordCombo, word, board) {
     let directions = [[-1, 0], [0, -1], [0, 1], [1, 0]]
     console.log(directions[0][0]);
     for (let k = 0; k < directions.length; k++) {
+
+        if (directions[k][0] + i > board.length && directions[k][1] + j > board[0].length && directions[k][0] + i < board.length && directions[k][1] + j < board[0].length) {
+            return false;
+        }
         let moveX = directions[k][0] + i;
         let moveY = directions[k][1] + j;
         let nextLetter = board[moveX][moveY];
---
+++ leet_code/994_rotting_oranges.js
@@ -0,0 +1,39 @@
+var orangesRotting = function(grid) {
+    let time = 0;
+    let directions = [[-1,0], [0, -1], [1, 0], [0, 1]];
+    let freshOranges = 0;
+    let rottenOranges = [];
+    
+    
+    for (let i = 0; i < grid.length; i++) {
+        for (let j = 0; j < grid[0].length; j++) {
+            if (grid[i][j] === 1) {
+                freshOranges++;
+            }
+            if (grid[i][j] === 2) {
+                rottenOranges.push([i,j])
+            }
+        }
+    }
+    
+    while (rottenOranges.length && freshOranges) {
+        let queue = [];
+        while(rottenOranges.length) {
+            let current = rottenOranges.pop();
+            for (let direction of directions) {
+                let row = current[0] + direction[0];
+                let col = current[1] + direction[1];
+                if (row >= 0 && col >= 0 && row < grid.length && col < grid[0].length) {
+                    if (grid[row][col] === 1) {
+                        grid[row][col] = 2;
+                        freshOranges -= 1;
+                        queue.push([row, col]);
+                    }
+                }
+            }
+        }
+        time++;
+        rottenOranges = queue;
+    }
+    return freshOranges ? -1 : time;
+};
