112fd010c4c4a7a05b6e806dfc1d43b3d028ec4b
Albert Chen <adc054@eng.ucsd.edu>
2021-03-01 19:38:29 -0800

    do practice problem

--- random_tests/test8.js
+++ random_tests/test8.js
@@ -31,3 +31,30 @@ function remove(string) {
 
 let string = 'ABBCCCBBCA'
 console.log(remove(string));
+
+
+function stringCompress(string) {
+    const stack = [];
+    const queue = string.split("");
+    while (queue.length > 0) {
+        if (stack.length === 0)  stack.push(queue.shift())
+        if (stack[stack.length - 1] !== queue[0] ) {
+            popStack(stack, queue)
+        } else if (stack[stack.length - 1] === queue[0]) {
+            stack.push(queue.shift())
+        }
+    }
+    if ( queue.length === 0 ) popStack(stack, queue)
+    return stack.join("")
+}
+function popStack(stack, queue) {
+    let lastEle = stack[stack.length - 1];
+    let secondToLast = stack[stack.length - 2];
+    let thirdToLast = stack[stack.length - 3];
+    if (lastEle === secondToLast && lastEle === thirdToLast ) {
+        let currentEle = lastEle
+        while (currentEle === stack[stack.length - 1]) stack.pop();
+    } else {
+        stack.push(queue.shift())
+    }
+}
