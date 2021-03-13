8831c500ecb6b9ddd7d73fc82f24fafbb990a975
Albert Chen <adc054@eng.ucsd.edu>
2021-03-13 12:18:39 -0800

    do practice problem

--- leet_code/movezero.js
+++ leet_code/movezero.js
@@ -13,15 +13,15 @@ function moveZeroes(nums) {
     return nums;    
 };
 
-// var moveZeroes = function(nums) {
-//   nums.sort((a,b) => {return a-b});
-//    for(let index = nums.length - 1; index >= 0 ; index--){
-//     if(nums[index] === 0){
-//       nums.splice(index, 1);
-//       nums.push(0);
-//     }
-//    }
-//    return nums;
-// };
+var moveZeroes = function(nums) {
+  nums.sort((a,b) => {return a-b});
+   for(let index = nums.length - 1; index >= 0 ; index--){
+    if(nums[index] === 0){
+      nums.splice(index, 1);
+      nums.push(0);
+    }
+   }
+   return nums;
+};
 
 console.log(moveZeroes([1,3,2,0,12 ,13]))
