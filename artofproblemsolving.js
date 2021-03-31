cfcf6ffed1f7ff30712ffe4b360158965787efec
Albert Chen <adc054@eng.ucsd.edu>
2021-03-31 13:35:02 -0700

    do challenge

---
+++ artofproblemsolving.js
@@ -0,0 +1,26 @@
+function solve(arr, target) {
+    for (let i = 0; i < arr.length; i++) {
+
+    }
+}
+
+let arr1 = [
+    [1], 
+    [4, 3], 
+    [3, 2, 6], 
+    [2, 9, 5, 2], 
+    [10, 5, 2, 15, 5]
+];
+
+let target1 = 720;
+//LRLL
+
+let arr2 = [
+    [1],
+    [2, 3],
+    [4, 1, 1]
+]
+
+let target2 = 2;
+//LR
+console.log(solve(arr2, target2));
